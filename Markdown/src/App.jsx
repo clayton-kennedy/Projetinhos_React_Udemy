e94ffea695d2cc267534a762d035701e6a47fbc0
Clayton Kennedy Passos dos Reis <Clayton.Reis@pg.df.gov.br>
2025-10-06 15:22:55 -0300

    Iniciando novo projeto de markdown

---
+++ Markdown/src/App.jsx
@@ -0,0 +1,14 @@
+import BarraFerramentas from "./components/BarraFerramentas"
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <BarraFerramentas />
+      <textarea name=""></textarea>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
---
+++ Markdown/src/components/BarraFerramentas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BarraFerramentas() {
+  return (
+    <div>BarraFerramentas</div>
+  )
+}
+
+export default BarraFerramentas
