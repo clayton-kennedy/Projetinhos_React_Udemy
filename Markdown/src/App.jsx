00cfa3ccb334c472fbdd07068d25054f365571ec
Clayton Kennedy Passos dos Reis <Clayton.Reis@pg.df.gov.br>
2025-10-07 11:50:24 -0300

    Finalização do app de markdown

--- Markdown/src/App.jsx
+++ Markdown/src/App.jsx
@@ -1,14 +1,42 @@
-import BarraFerramentas from "./components/BarraFerramentas"
+import { useState, useRef, useEffect } from "react";
+import { marked } from "marked";
+import BarraFerramentas from "./components/BarraFerramentas";
 
 function App() {
+  const [texto, setTexto] = useState(localStorage.getItem("markdownTexto") || "");
+  const textAreaRef = useRef(null);
+
+  const renderizarTexto = () => ({ __html: marked(texto) });
+
+  useEffect(() => {
+    localStorage.setItem("markdownTexto", texto);
+  }, [texto]);
+
+  const inserirTexto = (antes, depois) => {
+    const textArea = textAreaRef.current;
+    const inicio = textArea.selectionStart;
+    const fim = textArea.selectionEnd;
+    const textoCompleto = textArea.value;
+    const textoAnterior = textoCompleto.substring(0, inicio);
+    const textoSelecionado = textoCompleto.substring(inicio, fim);
+    const textoPosterior = textoCompleto.substring(fim);
+
+    const novoTexto = `${textoAnterior}${antes}${textoSelecionado}${depois}${textoPosterior}`;
+    setTexto(novoTexto);
+    textArea.focus();
+  };
 
   return (
     <div className="app-container">
-      <BarraFerramentas />
-      <textarea name=""></textarea>
-      <div></div>
+      <BarraFerramentas inserirTexto={inserirTexto} />
+      <textarea
+        ref={textAreaRef}
+        value={texto}
+        onChange={(e) => setTexto(e.target.value)}
+      />
+      <div className="preview" dangerouslySetInnerHTML={renderizarTexto()} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Markdown/src/components/BarraFerramentas.jsx
+++ Markdown/src/components/BarraFerramentas.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function BarraFerramentas() {
+function BarraFerramentas({ inserirTexto }) {
   return (
-    <div>BarraFerramentas</div>
-  )
+    <div className="toolbar">
+      <button onClick={() => inserirTexto("# ", "")}>h1</button>
+      <button onClick={() => inserirTexto("## ", "")}>h2</button>
+      <button onClick={() => inserirTexto("### ", "")}>h3</button>
+      <button onClick={() => inserirTexto("\n> ", "")}>Citação</button>
+      <button onClick={() => inserirTexto("**", "**")}>Bold</button>
+      <button onClick={() => inserirTexto("~~", "~~")}>Riscado</button>
+      <button onClick={() => inserirTexto("*", "*")}>Itálico</button>
+      <button onClick={() => inserirTexto("[", "](https://)")}>Link</button>
+      <button onClick={() => inserirTexto("\n```\n", "\n```\n")}>Code Block</button>
+      <button onClick={() => inserirTexto("\n- ", "")}>List Item</button>
+      <button onClick={() => inserirTexto("\n---\n", "")}>Horizontal Line</button>
+    </div>
+  );
 }
 
 export default BarraFerramentas
